45003e033899a33cdde75ccb4fd9cd351b9aff31
SAMUEL VERNECK <nodezeirojs@gmail.com>
2017-05-03 17:02:19 -0300

    limpo

--- resolucao/index-v4.js
+++ resolucao/index-v4.js
@@ -1,15 +1,1 @@
-var value = Math.random();
-var colors = [
-  'black',
-  'white'
-];
 
-style404BackgroundRandomly();
-
-function style404BackgroundRandomly() {
-  value > 0.5 ? styleBackground(0) : styleBackground(1);
-}
-
-function styleBackground(i) {
-  document.getElementById('page-not-found').style.background = colors[i];
-}
